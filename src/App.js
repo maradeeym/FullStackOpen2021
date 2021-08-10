bb6acecd52277c78f36d95ee1150faa414a74d9b
Martin <martin.kalliola@gmail.com>
2021-08-10 22:53:45 +0300

    step2

--- src/App.js
+++ src/App.js
@@ -5,10 +5,29 @@ const Header = (props) => {
     <h1> {props.course} </h1>
   )
 }
+
+const Part =(props) => {
+  
+  return(
+    <div>
+      <p>{props.content} {props.number}</p>
+    </div>
+
+  )
+}
+
 const Content = (props) => {
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
   return (
     <div>
-    <p>{props.content} {props.number}</p>
+    <Part content={part1} number={exercises1} />
+    <Part content={part2} number={exercises2} />
+    <Part content={part3} number={exercises3} />
     </div>
   )
 }
@@ -31,9 +50,7 @@ const App = () => {
   return (
     <div>
       <Header course={course}/>
-      <Content content={part1} number={exercises1}/>
-      <Content content={part2} number={exercises2}/>
-      <Content content={part3} number={exercises3}/>
+      <Content />
       <Total total={exercises1 + exercises2 + exercises3} />
     </div>
   )
