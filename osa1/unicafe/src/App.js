8f87567e89e2859d6a9c1ea0e5feaddc6a73b865
Martin Kalliola <martin.kalliola@gmail.com>
2021-08-24 08:51:28 +0300

    unicafe step1

---
+++ osa1/unicafe/src/App.js
@@ -0,0 +1,47 @@
+import React, { useState } from 'react'
+
+const Header = ({header}) => {
+  return(
+    <h1>{header}</h1>
+  )
+}
+const Button = (props) => {
+  return(
+    <button onClick={props.handleClick}>{props.text}</button>
+  )
+}
+
+const Stats = ({good, neutral, bad}) => {
+  return(
+    <div>
+      <li>good {good}</li>
+      <li>neutral {neutral}</li>
+      <li>bad {bad}</li>
+    </div>
+    
+  )
+}
+
+const App = () => {
+  // tallenna napit omaan tilaansa
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const plusGood = () => setGood(good + 1)
+  const plusNeutral = () => setNeutral(neutral + 1)
+  const plusBad = () => setBad(bad + 1)
+
+  return (
+    <div>
+      <Header header='give feedback'/>
+      <Button handleClick={plusGood} text='good'/>
+      <Button handleClick={plusNeutral} text='neutral'/>
+      <Button handleClick={plusBad} text='bad'/>
+      <h2>statistics</h2>
+      <Stats good={good} neutral={neutral} bad={bad}/>
+    </div>
+  )
+}
+
+export default App
