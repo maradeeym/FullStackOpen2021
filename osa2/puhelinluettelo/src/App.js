fbfd38d99e0117b3ed452338f1ef068b7791ed6e
Martin Kalliola <martin.kalliola@gmail.com>
2021-09-06 22:52:27 +0300

    step 3

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -3,6 +3,7 @@ import React, { useState } from 'react'
 const App = () => {
   const [ persons, setPersons] = useState([ ])
   const [ newName, setNewName ] = useState('')
+  const [ newNumber, setNewNumber ] = useState('')
 
   console.log(persons)
   console.log(newName)
@@ -12,6 +13,7 @@ const App = () => {
     console.log('button clicked', event.target)
     const nameObject = {
       name: newName,
+      number: newNumber,
       id: persons.length + 1,
     }
     if(persons.find(names => names.name === newName))
@@ -19,11 +21,17 @@ const App = () => {
     else
     setPersons(persons.concat(nameObject))
     setNewName('')
+    setNewNumber('')
   }
 
   const handleNameChange = (event) => {
     console.log(event.target.value)
     setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    console.log(event.target.value)
+    setNewNumber(event.target.value)
   }
 
   return (
@@ -37,11 +45,17 @@ const App = () => {
           />
         </div>
         <div>
+          number: <input
+          value={newNumber}
+          onChange={handleNumberChange}
+          />
+          </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(names => <ul key={names.id}>{names.name} </ul>)}  
+      {persons.map(contact => <ul key={contact.id}>{contact.name} {contact.number} </ul>)}  
     
     </div>
   )
