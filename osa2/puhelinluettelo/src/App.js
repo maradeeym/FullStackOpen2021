4fe2d446da34565f5cec87595d1ed4b8314a97ce
Martin Kalliola <martin.kalliola@gmail.com>
2021-09-08 20:31:42 +0300

    step4

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,16 +1,39 @@
 import React, { useState } from 'react'
 
 const App = () => {
-  const [ persons, setPersons] = useState([ ])
+  const [ persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456', id: 1},
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2},
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3},
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4}
+  ])
+
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
+  const [ findName, setFinder ] = useState('')
 
-  console.log(persons)
-  console.log(newName)
+  const personsToShow = persons.filter(person => person.name.toLowerCase().includes(findName))
+  console.log(personsToShow)
+
+  const Filter = () => {
+  const isBelowThreshold = (currentValue) => currentValue === 0;
+  const person = persons.map(contact => contact.name.toLowerCase().indexOf(findName))
+  console.log(person)
+  console.log(person.every(isBelowThreshold))
+
+  const filterIt = personsToShow.map(contact => <ul key={contact.id}>{contact.name} {contact.number}</ul> )
+  if(person.every(isBelowThreshold) === true)
+  return(
+  <p>Search something</p>
+  )
+  else
+  return(
+  <>{filterIt}</>
+  )
+  }
 
   const addNote = (event) => {
     event.preventDefault()
-    console.log('button clicked', event.target)
     const nameObject = {
       name: newName,
       number: newNumber,
@@ -25,12 +48,14 @@ const App = () => {
   }
 
   const handleNameChange = (event) => {
-    console.log(event.target.value)
     setNewName(event.target.value)
   }
 
+  const handleFinderChange = (event) => {
+    setFinder(event.target.value)
+  }
+
   const handleNumberChange = (event) => {
-    console.log(event.target.value)
     setNewNumber(event.target.value)
   }
 
@@ -38,6 +63,15 @@ const App = () => {
     <div>
       <h2>Phonebook</h2>
       <form onSubmit={addNote}>
+      <div>
+          filter shown with: <input 
+          value={findName}
+          onChange={handleFinderChange}
+          />
+        </div>
+      </form>
+        <h2>add new</h2>
+        <form onSubmit={addNote}>
         <div>
           name: <input 
           value={newName}
@@ -55,8 +89,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(contact => <ul key={contact.id}>{contact.name} {contact.number} </ul>)}  
-    
+      <Filter />
     </div>
   )
 
