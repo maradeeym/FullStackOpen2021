a4d064411bc818c316d8fda570ace925462e7eff
Martin Kalliola <martin.kalliola@gmail.com>
2021-09-30 16:41:39 +0300

    heroku uusi

--- index.js
+++ index.js
@@ -98,9 +98,10 @@ let time = new Date()
       }})
 
   
-const PORT = 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+      const PORT = process.env.PORT || 3001
+      app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`)
+      })
 
 
 
