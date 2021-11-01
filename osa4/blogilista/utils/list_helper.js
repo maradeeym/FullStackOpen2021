e215980b9c5a3fdabd24d945456f1a545a55f2a1
Martin Kalliola <martin.kalliola@gmail.com>
2021-11-01 20:51:49 +0200

    4.4 step

--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -1,7 +1,16 @@
 const dummy = (blogs) => {
     return (1)
   }
-  
+
+const totalLikes = (blogs) => {
+    const likes = blogs.map(blog => blog.likes)
+    const reducer = (sum, likes) => {
+        return sum + likes
+    }
+  return likes.reduce(reducer)
+}
+
   module.exports = {
-    dummy
+      dummy,
+      totalLikes
   }
