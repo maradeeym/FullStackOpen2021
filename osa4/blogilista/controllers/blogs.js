ab01e5dc4444f0e14ea794f8833f212c6ca36b52
Martin Kalliola <martin.kalliola@gmail.com>
2021-11-07 15:39:39 +0200

    4.14 step2

--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -28,6 +28,22 @@ blogsRouter.get('/:id', async (request, response) => {
     }
   })
 
+blogsRouter.put('/:id', (request, response, next) => {
+const body = request.body
+
+const blog = {
+    title: body.title,
+    author: body.author,
+    likes: body.likes,
+}
+
+Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
+    .then(updatedBlog => {
+      response.json(updatedBlog.toJSON())
+    })
+    .catch(error => next(error))
+})
+
 blogsRouter.delete('/:id', async (request, response) => {
     await Blog.findByIdAndRemove(request.params.id)
     response.status(204).end()
